3ca14c2bce896b1954d7350c1f6bbd88cf3d6e92
Willington <willington.martinez-blanco@epitech.eu>
2026-04-08 13:55:21 -0400

    Add: Create the productos const for all arrays of products

---
+++ js/main.js
@@ -0,0 +1,248 @@
+// Productos 
+
+const productos = [
+
+//  Coats
+
+{
+    id: "abrigo-01",
+    titulo: "Abrigo 01",
+    imagen: "./img/coats/01.jpg",
+    categoria: {
+
+        nombre: "Coats",
+        id: "coats"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "abrigo-02",
+    titulo: "Abrigo 02",
+    imagen: "./img/coats/02.jpg",
+    categoria: {
+
+        nombre: "Coats",
+        id: "coats"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "abrigo-03",
+    titulo: "Abrigo 03",
+    imagen: "./img/coats/03.jpg",
+    categoria: {
+
+        nombre: "Coats",
+        id: "coats"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "abrigo-04",
+    titulo: "Abrigo 04",
+    imagen: "./img/coats/04.jpg",
+    categoria: {
+
+        nombre: "Coats",
+        id: "coats"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "abrigo-04",
+    titulo: "Abrigo 04",
+    imagen: "./img/coats/04.jpg",
+    categoria: {
+
+        nombre: "Coats",
+        id: "coats"
+
+    },
+    precio: 1000
+
+},
+
+//Pants
+
+{
+    id: "Pantalon-01",
+    titulo: "Pantalon 01",
+    imagen: "./img/pants/01.jpg",
+    categoria: {
+
+        nombre: "Pants",
+        id: "pants"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Pantalon-02",
+    titulo: "Pantalon 02",
+    imagen: "./img/pants/02.jpg",
+    categoria: {
+
+        nombre: "Pants",
+        id: "pants"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Pantalon-03",
+    titulo: "Pantalon 03",
+    imagen: "./img/pants/03.jpg",
+    categoria: {
+
+        nombre: "Pants",
+        id: "pants"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Pantalon-04",
+    titulo: "Pantalon 04",
+    imagen: "./img/pants/04.jpg",
+    categoria: {
+
+        nombre: "Pants",
+        id: "pants"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Pantalon-05",
+    titulo: "Pantalon 05",
+    imagen: "./img/pants/05.jpg",
+    categoria: {
+
+        nombre: "Pants",
+        id: "pants"
+
+    },
+    precio: 1000
+
+},
+
+//Shirts
+
+{
+    id: "Camisa-01",
+    titulo: "Camisa 01",
+    imagen: "./img/shirt/01.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-02",
+    titulo: "Camisa 02",
+    imagen: "./img/shirt/02.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-03",
+    titulo: "Camisa 03",
+    imagen: "./img/shirt/03.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-04",
+    titulo: "Camisa 04",
+    imagen: "./img/shirt/04.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-05",
+    titulo: "Camisa 05",
+    imagen: "./img/shirt/05.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-06",
+    titulo: "Camisa 06",
+    imagen: "./img/shirt/06.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-07",
+    titulo: "Camisa 07",
+    imagen: "./img/shirt/07.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+{
+    id: "Camisa-08",
+    titulo: "Camisa 08",
+    imagen: "./img/shirt/08.jpg",
+    categoria: {
+
+        nombre: "Shirts",
+        id: "shirts"
+
+    },
+    precio: 1000
+
+},
+
+]
